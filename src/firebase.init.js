3f8aa9366d3d0909d7b11dd5926a45ccf1a72589
JugolKarmakar <jugol.cse@gmail.com>
2022-05-21 21:30:31 +0600

    first commit

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwV2hXGPPjg0Y3knVKxrREPin5fA22x6k",
+  authDomain: "manufacture-tools-product.firebaseapp.com",
+  projectId: "manufacture-tools-product",
+  storageBucket: "manufacture-tools-product.appspot.com",
+  messagingSenderId: "229767836513",
+  appId: "1:229767836513:web:86f6ea67001256e97b4b20",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
